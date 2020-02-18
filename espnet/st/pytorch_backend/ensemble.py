09a27df34b93eb5bf97ab48f9e2784c816dddee1
TitouanT <titouan.teyssier@gmail.com>
2020-02-18 16:45:47 +0100

    added docstrings

--- espnet/st/pytorch_backend/ensemble.py
+++ espnet/st/pytorch_backend/ensemble.py
@@ -1,6 +1,11 @@
 class Ensemble(STInterface, torch.nn.Module):
+    """Ensemble model for ST task
 
+    Args:
+        models (STInterface): a list of modele to aggreagate
+    """
     def __init__(self, models):
+        """ Construce an Ensemble object """
         super(Ensemble, self).__init__()
         torch.nn.Module.__init__(self)
 
@@ -9,6 +14,7 @@ class Ensemble(STInterface, torch.nn.Module):
         self.models = models
 
     def translate(self, x, trans_args, char_list=None, rnnlm=None):
+        """ Construce an Ensemble object """
         nbest_hyps = []
         for m in self.models:
             nbest_hyps.append(m.translate(x, trans_args, char_list, rnnlm))
@@ -16,6 +22,7 @@ class Ensemble(STInterface, torch.nn.Module):
         return nbest_hyps
 
     def translate_batch(self, x, trans_args, char_list=None, rnnlm=None):
+        """ Construce an Ensemble object """
         nbest_hyps = []
         for m in self.models:
             nbest_hyps.append(m.translate_batch(x, trans_args, char_list, rnnlm))
