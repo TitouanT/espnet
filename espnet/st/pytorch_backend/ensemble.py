dd8f11e4128763326732c39cd4c9cac2633ae264
TitouanT <titouan.teyssier@gmail.com>
2020-02-18 17:08:06 +0100

    fixed docstrings

--- espnet/st/pytorch_backend/ensemble.py
+++ espnet/st/pytorch_backend/ensemble.py
@@ -1,11 +1,20 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+"""Ensemble decoding definition for the speech translation task."""
+
+from espnet.nets.st_interface import STInterface
+import torch
 class Ensemble(STInterface, torch.nn.Module):
-    """Ensemble model for ST task
+    """Ensemble model for ST task.
 
     Args:
-        models (STInterface): a list of modele to aggreagate
+        models (STInterface): a list of modele to aggreagate.
+
     """
+
     def __init__(self, models):
-        """ Construce an Ensemble object """
+        """Construce an Ensemble object."""
         super(Ensemble, self).__init__()
         torch.nn.Module.__init__(self)
 
@@ -14,7 +23,7 @@ class Ensemble(STInterface, torch.nn.Module):
         self.models = models
 
     def translate(self, x, trans_args, char_list=None, rnnlm=None):
-        """ Construce an Ensemble object """
+        """Construce an Ensemble object."""
         nbest_hyps = []
         for m in self.models:
             nbest_hyps.append(m.translate(x, trans_args, char_list, rnnlm))
@@ -22,7 +31,7 @@ class Ensemble(STInterface, torch.nn.Module):
         return nbest_hyps
 
     def translate_batch(self, x, trans_args, char_list=None, rnnlm=None):
-        """ Construce an Ensemble object """
+        """Construce an Ensemble object."""
         nbest_hyps = []
         for m in self.models:
             nbest_hyps.append(m.translate_batch(x, trans_args, char_list, rnnlm))
