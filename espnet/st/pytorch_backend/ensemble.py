028bff89b3fb1b002c8ba18690d7660326017a86
TitouanT <titouan.teyssier@gmail.com>
2020-02-18 15:20:09 +0100

    added the Ensemble modele

---
+++ espnet/st/pytorch_backend/ensemble.py
@@ -0,0 +1,25 @@
+class Ensemble(STInterface, torch.nn.Module):
+
+	def __init__(self, models):
+		super(Ensemble, self).__init__()
+		torch.nn.Module.__init__(self)
+
+		for m in models:
+			self.add_module(m)
+		self.models = models
+
+	def translate(self, x, trans_args, char_list=None, rnnlm=None):
+		nbest_hyps = []
+		for m in self.models:
+			nbest_hyps.append(m.translate(x, trans_args, char_list, rnnlm))
+
+		return nbest_hyps
+
+	def translate_batch(self, x, trans_args, char_list=None, rnnlm=None):
+		nbest_hyps = []
+		for m in self.models:
+			nbest_hyps.append(m.translate_batch(x, trans_args, char_list, rnnlm))
+
+		# transpose
+		nbest_hyps = map(list, list(zip(*nbest_hyps)))
+		return nbest_hyps
